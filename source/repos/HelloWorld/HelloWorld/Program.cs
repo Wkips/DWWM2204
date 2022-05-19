1d1f7a96d0a34abe2fd99aa208e9932e2b01c0f4
Wkips <willy_kips@hotmail.fr>
2022-05-19 09:56:36 +0200

    exo
    exo algo et debut visualstudio

---
+++ source/repos/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
