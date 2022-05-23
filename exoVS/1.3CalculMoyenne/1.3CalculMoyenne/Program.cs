91c54f5218f15dcc5594931f084d54cd7acaee92
Wkips <willy_kips@hotmail.fr>
2022-05-23 17:00:28 +0200

    exo 1.3 moyenne ok

---
+++ exoVS/1.3CalculMoyenne/1.3CalculMoyenne/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int nombrea = 0;
+int nombreb;
+int resultat;
+
+Console.WriteLine("veuillez saisir le nombre a");
+nombrea = int.Parse(Console.ReadLine());
+
+Console.WriteLine("veuillez saisir nombre b");
+nombreb = int.Parse(Console.ReadLine());
+    
+
+
+Console.WriteLine("votre moyenne est de :" );
+Console.WriteLine((nombrea+nombreb)/2);
