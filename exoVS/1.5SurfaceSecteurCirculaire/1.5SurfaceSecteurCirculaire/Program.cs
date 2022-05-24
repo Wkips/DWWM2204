daf541c44ff43a66d412f2e945fe6ade0d9cebeb
Wkips <willy_kips@hotmail.fr>
2022-05-24 16:57:44 +0200

    1.5SecteurCirculaire

--- exoVS/1.5SurfaceSecteurCirculaire/1.5SurfaceSecteurCirculaire/Program.cs
+++ exoVS/1.5SurfaceSecteurCirculaire/1.5SurfaceSecteurCirculaire/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+double rayonr;
+double anglea;
+double airesecteurcirculaire;
+
+Console.WriteLine("saisir le rayon r");
+rayonr = double.Parse(Console.ReadLine());
+
+Console.WriteLine("saisir angle a ");
+anglea = double.Parse(Console.ReadLine());
+
+Console.WriteLine("l aire du secteur circulaire est ");
+airesecteurcirculaire = (Math.PI * Math.Pow(rayonr, 2) * anglea / 360 );
+Console.WriteLine(airesecteurcirculaire);
