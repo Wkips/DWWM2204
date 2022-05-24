133e2d7c4a0de0b418e08b40307243affa76d902
Wkips <willy_kips@hotmail.fr>
2022-05-24 16:44:22 +0200

    exo

---
+++ exoVS/1.4 CalculAireVolumeSphere/1.4 CalculAireVolumeSphere/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+double rayonr;
+double airea;
+double volumev;
+double aire;
+
+Console.WriteLine("Veuillez entrez le rayon r");
+rayonr = double.Parse(Console.ReadLine());
+
+Console.WriteLine("l'aire a est de:");
+aire= Math.Pow(rayonr, 2) * (Math.PI * 4);
+Console.WriteLine(aire);
+
+
+Console.WriteLine("le volume est de: ");
+volumev = ((4 / 3 * Math.PI) * Math.Pow(rayonr, 3));
+Console.WriteLine(volumev);  
