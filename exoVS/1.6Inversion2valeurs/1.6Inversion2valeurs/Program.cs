51825b7839df39e62d52224213315b24822143c9
Wkips <willy_kips@hotmail.fr>
2022-05-25 09:50:55 +0200

    1.6Inversion OK

---
+++ exoVS/1.6Inversion2valeurs/1.6Inversion2valeurs/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a;
+int b;
+int temp;
+Console.WriteLine("saisir a ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("saisir b");
+b = int.Parse(Console.ReadLine());
+
+temp = a;
+a = b;
+b = temp;
+Console.WriteLine("a= "+a);
+Console.WriteLine("b=" +b);
