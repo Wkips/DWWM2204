c9a3967bdea968b6258bc4446e9e5a6d8dc336af
Wkips <willy_kips@hotmail.fr>
2022-05-30 16:10:52 +0200

    Solution_AlgoKIPS
    Rangement et 2.2 es tu majeur OK

---
+++ exoVS/solutionAlgoKIPS/2_1TriNombres/2_1TriNombres/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Exercice 2.1 : Tri de nombres 
+
+//Lire 2 nombres entier A et B puis les afficher dans l’ordre croissant
+
+int a;
+int b;
+
+Console.WriteLine("veuillez saisir le nombre a ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("veuillez saisir le nombre b");
+b = int.Parse(Console.ReadLine());
+
+if (a > b) 
+{
+
+    Console.WriteLine(a + " est plus grand que " + b);
+}
+else 
+{
+
+    Console.WriteLine(b + " est plus grand que " + b);
+}
---
+++ exoVS/solutionAlgoKIPS/2_2EstTuMajeur/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Exercice 2.2 : Es-tu majeur ? 
+
+
+
+//Lire un nombre A correspondant à un âge (en années). 
+
+    //Afficher “Vous êtes majeur” ou “Vous êtes mineur” selon le cas. 
+
+      //  Pour un nombre négatif le message doit être “Vous n’êtes pas encore né”. 
+
+ 
+
+//La majorité est fixée à 18 ans
+
+    Console.WriteLine("es tu majeur ?");
+int age;
+Console.WriteLine("saisir votre age");
+age=int.Parse(Console.ReadLine());
+
+
+if (age >= 18)
+{
+    Console.WriteLine("Vous etes majeur");
+}
+else if (age <0 )
+{
+
+    Console.WriteLine("vous etes encore encore pas né");
+}
+else 
+{
+    Console.WriteLine("vous etes mineur");
+}
