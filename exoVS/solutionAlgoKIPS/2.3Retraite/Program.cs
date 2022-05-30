ba5ce7fa329d5522bd873961c3d9b2fe39caa3da
Wkips <willy_kips@hotmail.fr>
2022-05-30 17:01:13 +0200

    2.4trinombreStart

---
+++ exoVS/solutionAlgoKIPS/2.3Retraite/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+    /*    Lire un nombre A correspondant à un âge (en années). 
+
+    Selon l’âge fourni, le programme doit afficher l’un des messages suivants:
+
+        Vous êtes à la retraite depuis X années
+
+        Il vous reste X années avant la retraite.
+
+        C’est le moment de prendre sa retraite.
+
+        La valeur fournie n’est pas un âge valide. */
+int age;
+int ageRetraite;
+Console.WriteLine("saisir votre age");
+age=int.Parse(Console.ReadLine());
+ageRetraite = 60;
+
+if (age >ageRetraite)
+{
+    Console.WriteLine("vous etes a la retraite depuis "+ (age - ageRetraite) + " années");
+
+
+}
+else if (age <ageRetraite)
+{
+    Console.WriteLine("vous serez a la retraite dans" + (ageRetraite - age) +" années");
+}
+else
+{
+    Console.WriteLine("il est temps de partir en retraite ");
+}
+
---
+++ exoVS/solutionAlgoKIPS/2.4Tri_des_Nombres/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Lire 3 nombres entier A, B et C puis les afficher dans l’ordre décroissant.
+int a;
+int b;
+int c;
+
+Console.WriteLine("saisir le nombre a");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("saisir le nombre b");
+b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("saisir le nombre c");
+c = int.Parse(Console.ReadLine());
+
+if (a < b && b < c)
+{
+    Console.WriteLine( c+""+ b+""+ a);
+}
+
+
