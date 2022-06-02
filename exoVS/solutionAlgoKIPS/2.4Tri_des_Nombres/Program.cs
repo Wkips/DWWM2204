c6638ac94fcbfb076bc57c8d10ad4f50f9eb0962
Wkips <willy_kips@hotmail.fr>
2022-06-02 09:12:57 +0200

    2.5ok

--- exoVS/solutionAlgoKIPS/2.4Tri_des_Nombres/Program.cs
+++ exoVS/solutionAlgoKIPS/2.4Tri_des_Nombres/Program.cs
@@ -16,7 +16,31 @@ c = int.Parse(Console.ReadLine());
 
 if (a < b && b < c)
 {
-    Console.WriteLine( c+""+ b+""+ a);
+    Console.WriteLine(c + " " + b + " " + a);
+}
+else if (a < c && c < b)
+{
+    Console.WriteLine(b + " " + c + " " + a);
 }
 
+/*Sinon Si b<a et a<c 
 
+Afficher c,a,b*/
+else if (b < a && a< c)
+{ 
+    Console.WriteLine(c + " " + a + " " + b);
+
+}
+else if ( b<c  &&  c<a )
+{
+    Console.WriteLine(a + " " + c + "" + b);
+}
+
+else if ( c < a && a<b )
+{
+    Console.WriteLine(b + " " + a + " " + "" + c);
+}
+else if ( c<b && b<a )
+{
+    Console.WriteLine(a + " " + b + " " + " " + c);
+}
---
+++ exoVS/solutionAlgoKIPS/2.5Bicyclette_Bovary/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+string rep;
+
+
+Console.WriteLine("fait il beau aujourd'hui ? oui/non");
+rep = Console.ReadLine();
+if (rep == "oui")
+{
+    Console.WriteLine("le velo fonctionne t'il ? oui/non");
+    rep = Console.ReadLine();
+    if (rep == "oui")
+    {
+        Console.WriteLine("Aller faire une balade a vélo.");
+    }
+    else if (rep == "non")
+    {
+        Console.WriteLine("Au garage, les réparations peuvent-elles être fait immediatement ? ");
+        rep = Console.ReadLine();
+        if (rep == "oui")
+        {
+            Console.WriteLine("Faites une balade à vélo");
+        }
+       
+        else
+        {
+            Console.WriteLine("Avez-vous Game of throne?");
+            rep = Console.ReadLine();
+            if (rep == "oui")
+            {
+                Console.WriteLine("s'installer dans le fauteuil et commencer a lire");
+            }
+            else if (rep == "non")
+            {
+                Console.WriteLine("allez a la bibliotheque. Le livre GoT est il disponible?");
+               rep = Console.ReadLine();
+                if (rep == "oui")
+                {
+                    Console.WriteLine("rentrer a la maison et commencer a lire");
+                }
+                else if (rep == "non")
+                {
+                    Console.WriteLine("empruntez un livre policier rentrer a la maison et commencer a lire ");
+                }
+         
+           
+            }
+        }
+
+
+    }
+    
+
+    
+}
+else if (rep == "non")
+{
+Console.WriteLine("allez a pied cueuillir des joncs ");
+}
+else
+{
+
+}
+Console.WriteLine("Fin du programme");
