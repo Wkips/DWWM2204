57a73794287fff5a653a8a462fd112aa2932d255
Wkips <willy_kips@hotmail.fr>
2022-06-03 11:45:50 +0200

    3.2motdepasse

--- exoVS/solutionAlgoKIPS/3.1CtrlSaisie/Program.cs
+++ exoVS/solutionAlgoKIPS/3.1CtrlSaisie/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*L’utilisateur est invité à saisir son prénom.
+
+Le programme affiche ensuite “Bonjour” suivi du prénom saisi.
+
+Le prénom doit contenir au moins 2 caractères.
+
+Si le prénom contient moins de 2 caractères, l’utilisateur doit recommencer la saisie.
+
+L’utilisateur dispose d’un nombre d’essai illimité.*/
+
+string prenom;
+do
+{
+    Console.WriteLine("Veuillez saisir votre prenom");
+    prenom = Console.ReadLine();
+    if (prenom.Length < 2)
+    {
+        Console.WriteLine("saisir un prenom de deux lettres ");
+        prenom = Console.ReadLine();
+    }
+
+}
+while (prenom.Length < 2);
+
+
+
+        Console.WriteLine("bonjour " + prenom);
+
---
+++ exoVS/solutionAlgoKIPS/3.2Motdepasse/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Exercice 3.2 : Contrôler la saisie avec limite 
+
+L’utilisateur est invité à saisir un mot de passe. 
+
+Si le mot de passe saisi est correct, le programme affiche “Vous êtes connecté”. 
+
+Dans le cas contraire, l’utilisateur doit recommencer la saisie. 
+
+L’utilisateur dispose de 3 essais maximum. 
+
+Au 3ème échec, le programme affiche “Votre compte est bloqué” et se termine. 
+
+Note : Le bon mot de passe est formation */
+
+string pswd;
+int nombreTentativeLouppee;
+
+
+do
+{
+    Console.WriteLine("veuillez saisir votre mot de passe ");
+    pswd = Console.ReadLine();
+
+    if (pswd == "formation")
+
+    {
+        Console.WriteLine("vous etes connecte");
+    }
+    else
+    {
+        nombreTentativeLouppee++;
+    }
+}
+while (nombreTentativeLouppee > 3); 
+
+{ 
+    Console.WriteLine("Compte bloque"); 
+}
