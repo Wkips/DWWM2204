64e3c18706d6f54cf4565b87e293806530eb92b7
Wkips <willy_kips@hotmail.fr>
2022-06-09 09:17:41 +0200

    correction_depot

---
+++ exoVS/solutionAlgoKIPS/3.3Intervalle2Nombres/Program.cs
@@ -0,0 +1,30 @@
+﻿//Lire 2 nombres entier A et B puis afficher tous les nombres entiers dans l’intervalle. 
+
+int a;
+int b;
+int i;
+
+
+Console.WriteLine("Saisir le nombre a");
+a= int.Parse(Console.ReadLine());
+Console.WriteLine("Saisir le nombre b");
+b= int.Parse(Console.ReadLine());
+
+if (a < b)
+{
+    for (i = a; i < b; i++)
+    {
+        Console.WriteLine(i + " ");
+
+
+    }
+
+}
+else if (b<a)
+{
+    for (i = b; i < a; i++)
+    {
+        Console.WriteLine(i + "");
+    }
+
+}
---
+++ exoVS/solutionAlgoKIPS/3.4Diviseur/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Lire un nombre entier et afficher tous ses diviseurs autres que 1 et lui-même. 
+int a;
+int i;
+
+Console.WriteLine("saisir a ");
+a= int.Parse(Console.ReadLine());
+for (i = 1;i< a; i++)
+{
+    if (a%i == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
