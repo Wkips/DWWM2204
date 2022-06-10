eb470ab511795b718f1c442239f319ae5e0d04ae
Wkips <willy_kips@hotmail.fr>
2022-06-10 09:34:09 +0200

    3.5NombrePremierOK

---
+++ exoVS/solutionAlgoKIPS/3.5NombrePremier/Program.cs
@@ -0,0 +1,18 @@
+﻿//3.5 Nombre premier divisible par un et lui meme 
+int nombre1;
+string resultat ="est un nombre premier";
+Console.WriteLine("saisir nombre 1");
+nombre1 = int.Parse(Console.ReadLine());
+for (int i = 2; i < nombre1; i++)
+
+	{
+
+    if (nombre1 % i == 0)
+    {
+       resultat= nombre1+ " ce n est pas un nombre premoier";
+    }
+
+
+
+    }
+Console.WriteLine(resultat);
