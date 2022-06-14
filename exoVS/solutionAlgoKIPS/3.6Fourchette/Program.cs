92b0107ff5addb3eeec30a75796926111ddd159b
Wkips <willy_kips@hotmail.fr>
2022-06-14 16:53:03 +0200

    3.6fourchette
    en cours

--- exoVS/solutionAlgoKIPS/3.6Fourchette/Program.cs
+++ exoVS/solutionAlgoKIPS/3.6Fourchette/Program.cs
@@ -2,26 +2,44 @@
 //Le joueur essaie de le deviner. 
 
 Random rnd = new Random();
-int n = rnd.Next(0, 100);
+int nombreMachine = rnd.Next(0, 100);
 int choix;
+int min =0;
+int max =100;
 Console.WriteLine(n);
 
 do
 {
 
-    Console.WriteLine("veuillez choisir un nombre entre 0 et 100 ");
+    Console.WriteLine("veuillez choisir un nombre entre " + min + " et " + max);
     choix = int.Parse(Console.ReadLine());
-    if (choix == n)
+    if (choix == nombreMachine)
     {
         Console.WriteLine("vous avez gagné");
     }
     else
     {
         Console.WriteLine("try again");
-    }
-} while (choix !=n || n != -1);
+        if (choix < nombreMachine)
+        {
+            (min == choix);
+
+
+        }
+        else if (choix > nombreMachine)
+        {
+            (max == choix);
+        }
+
+        else
+        {
+            Console.Writeline("votre choix est compris entre " + min + " et " + max);
+        }
+
+    } while (choix != nombreMachine && nombreMachine != -1) ;
 
     Console.WriteLine("fin de partie ");
+}
         
 
 
