35e046233e611716eb2cb74b1d65f3a5cd824de3
Wkips <willy_kips@hotmail.fr>
2022-06-16 11:24:06 +0200

    cours

---
+++ exoVS/solutionAlgoKIPS/4.1RechercheNombreTab/Program.cs
@@ -0,0 +1,26 @@
+﻿//Exercice 4.1 : Rechercher un nombre dans un tableau
+
+//Soit un tableau de nombres entier triés par ordre croissant.
+
+//Exemple: [8, 16, 32, 64, 128, 256, 512]
+
+//Chercher si un nombre donné N figure parmi les éléments.
+
+//Si oui, afficher la valeur de l’indice correspondant. Sinon, afficher « Nombre non trouvé ».
+int[] tabnombre = new int[7];
+for (int i = 0; i < tabnombre.Length; i++)
+{
+    Console.WriteLine("veuillez entrer le nombre n°"+(i+1)+" du tableau:");
+    tabnombre[i] =int.Parse(Console.ReadLine());
+
+
+
+}
+
+Console.WriteLine("merci votre tableau est bien remplie voici son contenu :");
+
+foreach (int valeur in tabnombre)
+{
+    Console.Write(valeur + ",");
+}
+Console.ReadKey();
---
+++ exoVS/solutionAlgoKIPS/4.2RechercheLettrePhrase/Program.cs
@@ -0,0 +1,15 @@
+﻿//Exercice 4.2 : Rechercher une lettre dans une phrase
+
+//Soit une chaîne de caractères terminée par le caractère « . ».
+
+//Donnez l’algorithme d’un programme qui compte le nombre d’occurrences d’une lettre donnée (“a” par exemple) dans cette chaîne.
+
+//Si la chaîne de caractères est vide ou n’est composée que du caractère « . », le message “LA CHAINE EST VIDE” sera affiché.
+
+//Proposez un jeu d’essai prévoyant les 3 cas suivants :
+
+//    La phrase est vide
+//    La lettre n’est pas présente
+//    La lettre est présente une ou plusieurs fois
+
+String("hello World .");
