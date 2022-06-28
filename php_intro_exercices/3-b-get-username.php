9804c55d512277950632f0b7ab54f5f5071a38a4
Wkips <willy_kips@hotmail.fr>
2022-06-28 14:20:05 +0200

    create 03-string

---
+++ php_intro_exercices/3-b-get-username.php
@@ -0,0 +1,20 @@
+<?php
+//Créer une fonction « getUserName() » qui accepte deux arguments (prénom et nom) de type string.
+//Cette fonction doit retourner la concaténation des deux valeurs.
+//Exemple :
+//getUserName ('mickaël', 'devoldère'); // retourne « mickaëldevoldère »
+$name = readline("saisir votre nom :");
+$lastname = readline("saisir votre prenom : ");
+
+
+function getUserName(string $name, string $lastname)
+{
+    return $name . ' '. $lastname;
+}
+
+
+echo 'je m apelle : '.getUserName($name , $lastname);
+
+
+
+?>
