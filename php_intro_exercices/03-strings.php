ea7d57cd5733e7063869913b1e000fe6e0c10c27
Wkips <willy_kips@hotmail.fr>
2022-06-29 15:54:00 +0200

    3-d ok

--- php_intro_exercices/03-strings.php
+++ php_intro_exercices/03-strings.php
@@ -4,28 +4,30 @@
 //Créer une fonction « getMC2() ».
 //Cette fonction doit retourner Le nom de l’inventeur de la formule « E = MC² 
 
-echo 'Fonction getMC2'. PHP_EOL;
-function getMC2() {
-    return 'einstein';
+//echo 'Fonction getMC2'. PHP_EOL;
 
-}
 
 //3-b
 //Créer une fonction « getUserName() » qui accepte deux arguments (prénom et nom) de type string.
 //Cette fonction doit retourner la concaténation des deux valeurs.
 //Exemple :
 //getUserName ('mickaël', 'devoldère'); // retourne « mickaëldevoldère »
-$name = readline("saisir votre nom :");
-$lastname = readline("saisir votre prenom : ");
+//$name = readline("saisir votre nom :");
+//$lastname = readline("saisir votre prenom : ");
 
+
+function getMC2():string {
+    return 'einstein';
+
+}
 
 function getUserName(string $name, string $lastname)
 {
-    return $name . ' '. $lastname;
+    return $name . ' '. $lastname . ' ';
 }
 
 
-echo 'je m apelle : '.getUserName($name , $lastname);
+//echo 'je m apelle : '.getUserName($name , $lastname);
 
 
 //3c
@@ -39,13 +41,29 @@ echo 'je m apelle : '.getUserName($name , $lastname);
 //La conversion minuscule en majuscule se fait en PHP avec la fonction strtoupper() et l’inverse avec la fonction strtolower().
 
 
-$name=readline('saisir votre nom');
-$lastname=readline('saisir votre prenom');
+$name=readline('saisir votre nom ').PHP_EOL;
+$lastname=readline('saisir votre prenom ').PHP_EOL;
 function getFullName(string $name  ,  string $lastname)
 {
     return $name . ' ' . $lastname;
 }
 
-echo 'je m apelle : '.getFullName($name=strtoupper($name)  , $lastname=strtolower($lastname));
+//echo 'je m apelle : '.getFullName($name=strtoupper($name)  , $lastname=strtolower($lastname));
 
+//3d
+//Créer une fonction « askUser() » acceptant deux arguments (nom et prénom) de type string.
+//Cette fonction doit retourner une chaîne de caractères sous la forme :
+//« Bonjour prénom, nom. Connaissez-vous Einstein ? »
+//Cette fonction doit réutiliser les fonctions
+//« getFullName() » pour obtenir la concaténation des deux valeurs.
+// « getMC2() » pour obtenir le nom de l'inventeur de la formule E = MC².
+
+
+function askUser(string $name  ,  string $lastname) : string
+{
+    return 'Bonjour '.getFullName($name, $lastname) . ' connaissez vous :' . getMC2() . '?';
+
+}
+
+echo 'bonjour ' . askUser($name , $lastname);
 ?>
